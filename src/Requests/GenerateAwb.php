849ea329626512333b5e10ca6dd6568838d5cdfa
Maria Petrescu <mpetrescu@seniorprogramming.ro>
2018-06-15 12:08:49 +0300

    Fix parseResult on generateAwb

--- src/Requests/GenerateAwb.php
+++ src/Requests/GenerateAwb.php
@@ -7,64 +7,71 @@ use SeniorProgramming\FanCourier\Helpers\Hints;
 use SeniorProgramming\FanCourier\Exceptions\FanCourierInvalidParamException;
 
 class GenerateAwb extends Endpoint {
-    
+
     protected $keys;
-    
+
     /**
-     * 
+     *
      * @return string
      */
     protected function getCallMethod()
     {
         return 'import_awb_integrat.php';
     }
-    
+
     /**
-     * 
+     *
      * @return string
      */
     public function fetchResults() {
         return 'parse';
     }
-    
+
     /**
-     * 
+     *
      * @param string $result
      * @return int|string
      */
-    public function parseResult($result) 
+    public function parseResult($result)
     {
         $parse = str_getcsv($result, "\n");
         $return_result = [];
         if (empty($parse)) {
             return $result;
         }
-        
+
         try {
             foreach ($parse as $value) {
                 $result_per_row = explode(',', $value);
-                $return_result[] = (object) ['line' => $result_per_row[0], 
-                    'awb' => $result_per_row[1] == 1 ? $result_per_row[2] : false, 
-                    'send_params' => current(array_values($this->keys))[$result_per_row[0]],
-                    'error_message' => $result_per_row[1] == 1 ? '' : $result_per_row[2] ];
+                if (empty($result_per_row[0]) && empty($result_per_row[1]) && empty($result_per_row[2])) {
+                    continue;
+                }
+                $return_result[] = (object) ['line' => (int) $result_per_row[0],
+                        'awb' => $result_per_row[1] == 1 ? $result_per_row[2] : false,
+                        'send_params' => current(array_values($this->keys))[(int) $result_per_row[0]],
+                        'error_message' => $result_per_row[1] == 1 ? '' : $result_per_row[2]];
             }
         } catch (Exception $ex) {
-
+            $return_result[] = (object) ['line' => 1,
+                    'awb' => false,
+                    'send_params' => false,
+                    'error_message' => $ex->getMessage()
+            ];
         }
-        
+
         return $return_result;
     }
-    
+
     /**
-     * 
+     *
      * @return boolean
      */
     public function convertInCsv() {
         return Hints::importCsvValues();
     }
-    
+
     /**
-     * 
+     *
      * @param array $params
      * @return boolean
      * @throws FanCourierInvalidParamException
@@ -77,7 +84,7 @@ class GenerateAwb extends Endpoint {
             if (empty($params[$key])) {
                 throw new FanCourierInvalidParamException('Must define the following key: ' . $key . '. ' ) ;
             }
-            
+
             $i = 1;
             foreach ($params[$key] as $val) {
                 if (empty($val) || !is_array($val) || (count(array_diff($keys_hint, array_keys($val))) != 0 && count(array_diff($keys_hint, array_keys($val))) > (count($keys_hint) - 1))) {
@@ -86,25 +93,25 @@ class GenerateAwb extends Endpoint {
                 }
                 foreach($keys_hint as $v) {
                     $this->keys[$key][$i][$v] = !empty($val[$v]) ? $val[$v] : '';
-                    
+
                 }
                 $i++;
             }
         }
         return true;
-        
+
     }
-    
+
     public function getParams()
     {
         return $this->keys;
     }
-    
+
     /**
-     * 
+     *
      * @return array
      */
-    private function methodRequirements() 
+    private function methodRequirements()
     {
         return [
             'fisier', //acesta este fisierul care contine datele despre expeditiile de importat;fisierul se creeaza conform modelului pentru import AWB-uri in aplicatia FAN, poate contine una sau mai multe inregistrari (expeditii) si poate fi descarcat din aplicatie.
